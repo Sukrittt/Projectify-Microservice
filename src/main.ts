093e2e3c047d8d968e49c312919cf789b3c19900
sukrittt <sukritsaha27@gmail.com>
2024-11-28 12:23:45 +0530

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import * as cors from 'cors';
+import * as express from 'express';
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(express.json());
+  app.use(cors());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Projectify Microservice')
+    .setDescription('API Documentation for Projectify Microservice')
+    .setVersion('1.0')
+    .addTag('projectify')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 4000);
+}
+bootstrap();
